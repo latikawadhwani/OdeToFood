d21dffcded279f69cd1bd4ef861a5ac5e3eb361b
LatikaW <latikawadhwani@gmail.com>
2017-08-13 15:46:02 -0700

    Added code to create a restaurant

--- src/OdeToFood/Controllers/HomeController.cs
+++ src/OdeToFood/Controllers/HomeController.cs
@@ -1,24 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using OdeToFood.Models;
+using OdeToFood.Entities;
 using OdeToFood.Services;
+using OdeToFood.ViewModels;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
         public IRestaurantData _restaurantData;
-        public HomeController(IRestaurantData restaurantData)
+        public IGreeter _greeter;
+
+        public HomeController(IRestaurantData restaurantData, IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
         public IActionResult Index()
         {
-            var model = _restaurantData.GetAll();
-
+            var model = new HomePageViewModel();
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentMessage = "";
             return View(model); 
             
         }
--- src/OdeToFood/Services/RestaurantData.cs
+++ src/OdeToFood/Services/RestaurantData.cs
@@ -1,8 +1,7 @@
-﻿using OdeToFood.Models;
+﻿using OdeToFood.Entities;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace OdeToFood.Services
 {
